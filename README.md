9ffb3883ad7558e62ce0d2f4b67bb938e035b00e
Denis Bystruev <dbystruev@me.com>
2021-05-18 21:05:35 +0300

    Added acoola_categories and renamed categories to farfetch_categories

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 Utilities for working with Get Outfit server written in [Go](https://golang.org).
 
-* [categories.go](categories.go) Get the list of categories from the [server](https://github.com/dbystruev/Get-Outfit-Server.git), then get the number of offers in each category.
+* [acoola_categories.go](acoola_categories.go) Get the list of Acoola categories from the [server](https://github.com/dbystruev/Get-Outfit-Server.git), then get the number of offers in each category.
+* [farfetch_categories.go](farfetch_categories.go) Get the list of Farfetch categories from the [server](https://github.com/dbystruev/Get-Outfit-Server.git), then get the number of offers in each category.
 * [cache.go](cache.go) Request items for female, male, and non-binary categories from the [server](https://github.com/dbystruev/Get-Outfit-Server.git) to pre-cache [client app](https://github.com/dbystruev/Outfit-Selection.git)'s requests.
 
 ## Run
 ```bash
-go run categories.go
+go run acoola_categories.go
+go run farfetch_categories.go
 go run cache.go
 ```
---
+++ acoola_categories.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// Category is the model for items' categories
+type Category struct {
+	ID int
+	Name string
+	//ParentID int
+}
+
+// Count is the model for JSON counters
+type Count struct {
+	Count int
+}
+
+// Offer is the model for the item
+//type Offer struct {
+//	Name string
+//}
+
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return true
+	}
+	return false
+}
+
+// https://stackoverflow.com/a/31129967
+func get(url string, target interface{}) error {
+	resp, err := http.Get(url)
+
+	if isError(err) { return err }
+
+	defer func() {
+		err = resp.Body.Close()
+		if isError(err) { return }
+	}()
+
+	err = json.NewDecoder(resp.Body).Decode(target)
+
+	if isError(err) { return err }
+
+	return err
+}
+
+func main() {
+	categories := make([]Category, 0)
+
+	err := get("http://spb.getoutfit.co:8888/categories?limit=-1", &categories)
+
+	if err != nil { return }
+
+	printCategories(categories)
+}
+
+func printCategories(categories []Category) {
+	fmt.Println("Number of categories:", len(categories))
+
+	offers := 0
+
+	for index, category := range categories {
+		offers += printCategory(index, category)
+	}
+
+	fmt.Println("Number of offers in categories:", offers)
+
+	counter := Count{}
+
+	if get("http://spb.getoutfit.co:8888/offers?count=true&limit=-1", &counter) != nil { return }
+
+	fmt.Println("Total number of offers:", counter.Count)
+}
+
+func printCategory(index int, category Category) int {
+	fmt.Print(index, " Category id: ", category.ID, ", name: ", category.Name, ", offers: ")
+
+	counter := Count{}
+	url := fmt.Sprintf("http://spb.getoutfit.co:8888/offers?categoryId=%d&count=true&limit=-1", category.ID)
+
+	if get(url, &counter) != nil {
+		fmt.Println("none")
+		return 0
+	}
+
+	fmt.Println(counter.Count)
+
+	return counter.Count
+}
